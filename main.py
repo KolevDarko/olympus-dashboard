491cc336c6479917369d9a75245a268f28e3b2b4
Darko Kolev <kolevdarko@gmail.com>
2022-03-11 16:27:01 +0100

    cleaned things up

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import os
 from web3 import Web3
 from contracts_abi import CUSTOM_TREASURY_ABI, CUSTOM_BOND_ABI, ERC20_ABI
 import requests
 from decimal import Decimal
 
-INFURA_URL = "https://mainnet.infura.io/v3/71f5f61283a047f68ea209ff01f4a7c1"
-CMC_API_KEY = "a98770be50430d344543261b2485f86a55f571e6db87d2c4b84fbda4c6598070"
-web3 = Web3(Web3.HTTPProvider(INFURA_URL))
+INFURA_API_KEY = os.getenv('INFURA_API_KEY')
+COINMARKETCAP_API_KEY = os.getenv('CMC_API_KEY')
+
+# INFURA_URL = f"https://mainnet.infura.io/v3/{INFURA_API_KEY}"
+# CMC_API_KEY = "a98770be50430d344543261b2485f86a55f571e6db87d2c4b84fbda4c6598070"
+web3 = Web3(Web3.HTTPProvider(INFURA_API_KEY))
 
 
 def extract_events(tx_hash):
@@ -22,23 +26,19 @@ def extract_events(tx_hash):
     }
 
 
-def get_payout_token(custom_treasury_address):
+def get_payout_token_data(custom_treasury_address):
     custom_treasury_contract = web3.eth.contract(address=web3.toChecksumAddress(custom_treasury_address),
                                                  abi=CUSTOM_TREASURY_ABI)
     token_address = custom_treasury_contract.functions.payoutToken().call()
-    return token_address
-
-
-def get_token_price(token_address):
     token_contract = web3.eth.contract(token_address, abi=ERC20_ABI)
     token_symbol = token_contract.functions.symbol().call()
-    coin_market_cap_data = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={token_symbol}&tsyms=USD&api_key={CMC_API_KEY}')
+    coin_market_cap_data = requests.get(
+        f'https://min-api.cryptocompare.com/data/price?fsym={token_symbol}&tsyms=USD&api_key={COINMARKETCAP_API_KEY}')
     token_usd_price = coin_market_cap_data.json()['USD']
-    return token_usd_price
-
-
-def get_final_payout(token_amount, token_address):
-    return token_amount * Decimal(get_token_price(token_address))
+    return {
+        'price': token_usd_price,
+        'symbol': token_symbol
+    }
 
 
 if __name__ == '__main__':
@@ -47,6 +47,8 @@ if __name__ == '__main__':
     payout_token_amount = all_events['bondCreated'][0]['args']['payout']
     full_tokens = web3.fromWei(payout_token_amount, 'ether')
     custom_treasury_address = all_events['transfers'][0]['args']['from']
-    payout_token_address = get_payout_token(custom_treasury_address)
-    payout_dollars = get_final_payout(full_tokens, payout_token_address)
-    print(f'Payout tokens were {full_tokens}, in value of ${payout_dollars}')
+    payout_token_data = get_payout_token_data(custom_treasury_address)
+    payout_token_symbol = payout_token_data['symbol']
+    usd_price = payout_token_data['price']
+    total_usd_payout = payout_token_amount * Decimal(usd_price)
+    print(f'Payout tokens were {full_tokens} {payout_token_symbol}, in value of ${total_usd_payout}')
